23f453187d5f61b6d05cb07052629b8964d88e03
mhsong <77idra77@gmail.com>
2023-03-29 16:11:48 +0900

    프로젝트 파일 업로드

---
+++ Source/ProjectSound/ProjectSoundGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectSoundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSOUND_API AProjectSoundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

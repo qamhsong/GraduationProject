558df8fce13abd34792038ab26fe5a1d78aa67d9
mhsong <77idra77@gmail.com>
2023-05-11 16:52:50 +0900

    ui widget 세팅

---
+++ Source/ProjectSound/UI_AudioOption.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI_Base.h"
+#include "UI_AudioOption.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSOUND_API UUI_AudioOption : public UUI_Base
+{
+	GENERATED_BODY()
+	
+};
